2fc5b83d3d65e98e69fa4501bbc492f92854efd0
Kalamojo <kojo22444488888888@gmail.com>
2026-01-13 15:21:54 -0500

    Last optimization attempt (surely)

--- download_model.py
+++ download_model.py
@@ -2,7 +2,7 @@ from huggingface_hub import hf_hub_download
 import os
 
 repo_id = "laion/CLIP-ViT-B-32-256x256-DataComp-s34B-b86K"
-filename = "open_clip_pytorch_model.bin"
+filename = "open_clip_model.safetensors"
 local_directory = "clip_model"
 
 os.makedirs(local_directory, exist_ok=True)
