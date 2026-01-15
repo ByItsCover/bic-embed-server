6a54894700fb04671b6e9735b2dbd0cd233d0b08
Kalamojo <kojo22444488888888@gmail.com>
2026-01-15 17:11:17 -0500

    Copying over model from docker

--- bootstrap.py
+++ bootstrap.py
@@ -1,43 +1,48 @@
 import os
 import sys
+import shutil
 
-def force_ipv4():
-    # Monkey patch to force IPv4, since FB seems to hang on IPv6
-    import socket
+# def force_ipv4():
+#     # Monkey patch to force IPv4, since FB seems to hang on IPv6
+#     import socket
 
-    old_getaddrinfo = socket.getaddrinfo
-    def new_getaddrinfo(*args, **kwargs):
-        responses = old_getaddrinfo(*args, **kwargs)
-        return [response
-                for response in responses
-                if response[0] == socket.AF_INET]
-    socket.getaddrinfo = new_getaddrinfo
+#     old_getaddrinfo = socket.getaddrinfo
+#     def new_getaddrinfo(*args, **kwargs):
+#         responses = old_getaddrinfo(*args, **kwargs)
+#         return [response
+#                 for response in responses
+#                 if response[0] == socket.AF_INET]
+#     socket.getaddrinfo = new_getaddrinfo
 
-force_ipv4()
-from huggingface_hub import hf_hub_download
+# force_ipv4()
+# from huggingface_hub import hf_hub_download
 
 
 def handler(event, context):
-    repo_id = "laion/CLIP-ViT-B-32-256x256-DataComp-s34B-b86K"
+    #repo_id = "laion/CLIP-ViT-B-32-256x256-DataComp-s34B-b86K"
     filename = "open_clip_model.safetensors"
+    source = os.path.join(os.environ.get('LAMBDA_TASK_ROOT', '.'), "clip_model", filename)
     destination = os.path.join(event.get("destination"), "clip_model")
 
     os.makedirs(destination, exist_ok=True)
 
-    hf_hub_download(
-        repo_id=repo_id,
-        filename=filename,
-        local_dir=destination
-    )
+    shutil.copy_file(source, os.path.join(destination, filename))
 
-    print(f"Model {repo_id} downloaded to {destination}/")
+    # hf_hub_download(
+    #     repo_id=repo_id,
+    #     filename=filename,
+    #     local_dir=destination
+    # )
+
+    #print(f"Model {repo_id} downloaded to {destination}/")
+    print(f"Model {filename} copied to {destination}/")
     return {"status": "success", "path": destination}
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        raise ValueError("Destination path is missing")
+# if __name__ == "__main__":
+#     if len(sys.argv) < 2:
+#         raise ValueError("Destination path is missing")
 
-    event = {"destination": sys.argv[1]}
-    response = handler(event, None)
-    print("Response:")
-    print(response)
+#     event = {"destination": sys.argv[1]}
+#     response = handler(event, None)
+#     print("Response:")
+#     print(response)
