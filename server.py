989473c96d4b65c910ac7d9bdbc4dfbbcc6ddcf7
Kalamojo <kojo22444488888888@gmail.com>
2026-01-14 19:49:14 -0500

    Moving imports to start

--- server.py
+++ server.py
@@ -2,13 +2,17 @@ from mangum import Mangum
 
 from fastapi import FastAPI, Depends
 from contextlib import asynccontextmanager
-from fastapi_injectable.util import async_get_injected_obj
+from fastapi_injectable.util import async_get_injected_obj, get_injected_obj
 
 import asyncio
 from aiohttp import ClientSession
 from pydantic import BaseModel
 from typing import Optional
 import os
+#import time
+
+import torch
+import open_clip
 
 from helpers import retrieve_images, process_images, get_embeddings
 
@@ -39,9 +43,11 @@ async def root():
     return {"message": "Hello World"}
 
 
-async def load_clip():
+def load_clip():
     print("loading clip...")
-    import open_clip
+    #await asyncio.sleep(10)
+    #time.sleep(10)
+    #import open_clip
 
     print("Pretrained path:", app_state["pretrained_name"])
     clip_model, _, preprocess = open_clip.create_model_and_transforms(
@@ -52,25 +58,27 @@ async def load_clip():
 
     return clip_model, preprocess
 
-async def load_torch():
-    print("loading torch...")
-    import torch
-    return torch
+# def load_torch():
+#     #time.sleep(10)
+#     print("loading torch...")
+#     import torch
+#     return torch
 
 class EmbedRequest(BaseModel):
     image_urls: list[Optional[str]] = []
 
 @app.post("/predict")
 async def predict(embed_request: EmbedRequest):
-    clip_task = asyncio.create_task(async_get_injected_obj(load_clip))
-    torch_task = asyncio.create_task(async_get_injected_obj(load_torch))
+    clip_task = asyncio.to_thread(get_injected_obj, load_clip)
+    #torch_task = asyncio.to_thread(get_injected_obj, load_torch)
 
+    #clip, torch, raw_images = await asyncio.gather(clip_task, torch_task, retrieve_images(embed_request.image_urls, app_state["session"]))
     clip, raw_images = await asyncio.gather(clip_task, retrieve_images(embed_request.image_urls, app_state["session"]))
     clip_model, preprocess = clip
 
     processed_images, was_processed = process_images(preprocess, raw_images, app_state["device"])
 
-    torch = await torch_task
+    #torch = await torch_task
     images_tensor = torch.cat(processed_images, dim=0) if processed_images else None
 
     image_embeddings = get_embeddings(images_tensor, was_processed, clip_model, torch)
