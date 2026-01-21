cf72422b35cc7e178f015c4cc4badef080e44c7a
Kalamojo <kojo22444488888888@gmail.com>
2026-01-21 12:05:53 -0500

    Init time optimizations

--- download_model.py
+++ download_model.py
@@ -44,10 +44,7 @@ def quantized_download(destination: str, clean_cache: bool = True):
             destination,
             "preprocess.onnx"
         ),
-        "device": "cpu",
-        # "interpolation_mode": T.InterpolationMode.BICUBIC,
-        # "transform_mean": OPENAI_DATASET_MEAN,
-        # "transform_std": OPENAI_DATASET_STD,
+        "device": "cpu"
     }
 
     os.makedirs(destination, exist_ok=True)
@@ -64,13 +61,6 @@ def quantized_download(destination: str, clean_cache: bool = True):
     print(clip_model)
 
 
-    # print("Exporting Preprocess...")
-
-    # torch.save(preprocess, script_state["preprocess_path"])
-
-    #print(preprocess)
-
-
     print("Exporting model to onnx format...")
 
     torch.onnx.export(clip_model.visual,
@@ -78,64 +68,12 @@ def quantized_download(destination: str, clean_cache: bool = True):
                   script_state["onnx_model_path"],
                   input_names = ['input'],
                   output_names = ['output'],
-                  #dynamic_shapes = {'x': {0: torch.export.Dim("batch", min=1, max=1024)}}
                   dynamic_axes={
-                    'input': {0: 'batch_size'},
-                    'output': {0: 'batch_size'}
-                },
-                dynamo=False
+                      'input': {0: 'batch_size'},
+                      'output': {0: 'batch_size'}
+                  },
+                  dynamo=False
                   )
-
-    # from onnxsim import simplify
-
-    # model_simp, check = simplify(onnx_model.model)
-    # if check:
-    #     onnx.save(model_simp, script_state["onnx_model_path"])
-
-    # Iterate through all nodes to find Concat nodes
-    #print("doin something")
-    #print(onnx_model.model.graph.node)
-
-    # print(onnx_model.model.graph.node.len())
-    # concat_nodes = []
-    # for node in onnx_model.model.graph.node():
-    #     if node.op_type == 'Concat':
-    #         concat_nodes.append(node)
-
-    # print(concat_nodes)
-
-    # print("Exporting Preprocess to ONNX...")
-
-    # class PreprocessModule(torch.nn.Module):
-    #     def __init__(self):
-    #         super().__init__()
-    #         self.transform = T.Compose([
-    #             T.Resize((224, 224), interpolation=script_state["interpolation_mode"]),
-    #             T.CenterCrop((224, 224)),
-    #             #MaybeToTensor(),
-    #             #T.Normalize(mean=script_state["transform_mean"], std=script_state["transform_std"])
-    #         ])
-
-    #     def forward(self, x: torch.Tensor):
-    #         x = self.transform(x)
-    #         return (x - script_state["transform_mean"]) / script_state["transform_std"]
-
-
-    # torch.onnx.export(PreprocessModule(),
-    #               (torch.ones(1, 3, 224, 224),),
-    #               script_state["preprocess_path"],
-    #               input_names = ['input'],
-    #               output_names = ['output'],
-    #               dynamic_shapes = {
-    #                     'x': {
-    #                         0: 'batch_size',
-    #                         2: 'height',
-    #                         3: 'width'
-    #                     },
-    #                     # 'output': {
-    #                     #     0: 'batch_size'
-    #                     # }
-    #                 })
 
 
     print("Quantizing model...")
--- server.py
+++ server.py
@@ -2,45 +2,32 @@ from mangum import Mangum
 
 from fastapi import FastAPI#, Depends
 from contextlib import asynccontextmanager
-from fastapi_injectable.util import get_injected_obj
 
-import asyncio
-from aiohttp import ClientSession
 from pydantic import BaseModel
 from typing import Optional
-import os
-#import time
 
 import numpy as np
-
-#import torch
-#import open_clip
-
-from helpers import retrieve_images, process_images, get_embeddings
 
 
 app_state = {}
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("Loading state...")
+    from aiohttp import ClientSession
+    import os
+
     source_path = os.environ.get('LAMBDA_TASK_ROOT', '.')
     app_state["model_name"] = "ViT-B-32"
     app_state["pretrained_name"] = os.path.join(
             source_path,
             "clip_model/clip_quantized.onnx"
         )
-    app_state["preprocess_path"] = os.path.join(
-            source_path,
-            "clip_model/preprocess.pt"
-        )
     app_state["image_width"] = 224
     app_state["image_height"] = 224
     app_state["transform_mean"] = np.array([0.48145466, 0.4578275, 0.40821073])
     app_state["transform_std"] = np.array([0.26862954, 0.26130258, 0.27577711])
-    #app_state["pretrained_name"] = "laion2b_s34b_b79k"
-    app_state["device"] = "cpu"
     app_state["session"] = ClientSession()
+    print("Loaded state")
     yield
     await app_state["session"].close()
     app_state.clear()
@@ -55,54 +42,41 @@ async def root():
 
 
 def load_clip():
-    print("loading clip (and torch)...")
-    #await asyncio.sleep(10)
-    #time.sleep(10)
-    #import open_clip
-    #import torch
-    from onnxruntime import InferenceSession
+    import onnxruntime as ort
 
-    print("Pretrained path:", app_state["pretrained_name"])
-    # clip_model, _, preprocess = open_clip.create_model_and_transforms(
-    #         app_state["model_name"], 
-    #         pretrained=app_state["pretrained_name"], 
-    #         device=app_state["device"]
-    #     )
-    clip_session = InferenceSession(app_state["pretrained_name"])
-    print("Clip session inputs:")
-    print(clip_session.get_inputs())
-    # preprocess = torch.load(
-    #         app_state["preprocess_path"],
-    #         weights_only=False
-    #     )
+    opts = ort.SessionOptions()
+    opts.intra_op_num_threads = 1
+    opts.inter_op_num_threads = 1
+    opts.execution_mode = ort.ExecutionMode.ORT_SEQUENTIAL
 
+    clip_session = ort.InferenceSession(app_state["pretrained_name"], opts, providers=["CPUExecutionProvider"])
     print("Loaded all of CLIP")
 
     return clip_session
-
-# def load_torch():
-#     #time.sleep(10)
-#     print("loading torch...")
-#     import torch
-#     return torch
 
 class EmbedRequest(BaseModel):
     image_urls: list[Optional[str]] = []
 
 @app.post("/predict")
 async def predict(embed_request: EmbedRequest):
+    import asyncio
+    from fastapi_injectable.util import get_injected_obj
+    from helpers import retrieve_images, process_images, get_embeddings
+
     clip_task = asyncio.to_thread(get_injected_obj, load_clip)
-    #torch_task = asyncio.to_thread(get_injected_obj, load_torch)
+    clip_session, raw_images = await asyncio.gather(
+            clip_task, 
+            retrieve_images(embed_request.image_urls, app_state["session"])
+        )
 
-    #clip, torch, raw_images = await asyncio.gather(clip_task, torch_task, retrieve_images(embed_request.image_urls, app_state["session"]))
-    clip_session, raw_images = await asyncio.gather(clip_task, retrieve_images(embed_request.image_urls, app_state["session"]))
-    #clip_session, preprocess = clip
-
-    processed_images, was_processed = process_images(raw_images, app_state)
-
-    #torch = await torch_task
+    processed_images, was_processed = process_images(
+            raw_images, 
+            app_state["image_width"], 
+            app_state["image_height"], 
+            app_state["transform_mean"], 
+            app_state["transform_std"]
+        )
     images_array = np.concatenate(processed_images, axis=0) if processed_images else None
-
     image_embeddings = get_embeddings(images_array, was_processed, clip_session)
 
     return {
