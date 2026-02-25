9f390646b43c443fd4900791caa4219e17a1035b
Kalamojo <kojo22444488888888@gmail.com>
2026-02-25 00:40:46 -0500

    Adding required cover fields

--- models.py
+++ models.py
@@ -8,6 +8,8 @@ class EmbedRecord(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     message_id: str = Field(alias='messageId')
+    cover_id: str
+    isbn_13: str
     image_url: str = Field(validation_alias=AliasPath('body', 'image_url'))
     raw_image: Optional[Image.Image] = None
     image_array: Optional[NDArray] = None
@@ -18,5 +20,5 @@ class BatchFailures(BaseModel):
 class Cover(LanceModel):
     cover_id: int
     isbn_13: str
-    cover_url: str
+    cover_url: str = Field(alias='image_url')
     embedding: Vector(512) #pyright: ignore[reportInvalidTypeForm]
