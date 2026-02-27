f3319e8b711c41f221b3253eed5c6962a799185e
Kalamojo <kojo22444488888888@gmail.com>
2026-02-27 00:28:41 -0500

    Fixing body reference

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class EmbedRecord(BaseModel):
     message_id: str = Field(alias='messageId')
     cover_id: int = Field(validation_alias=AliasPath('messageAttributes', 'cover_id'))
     isbn_13: str = Field(validation_alias=AliasPath('messageAttributes', 'isbn_13'))
-    image_url: str = Field(alias='messageId')
+    image_url: str = Field(alias='body')
     raw_image: Optional[Image.Image] = None
     image_array: Optional[NDArray] = None
 
