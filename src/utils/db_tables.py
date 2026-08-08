05dd2faf2702cf88a0c04d58eff4fc6d17c7de5c
Kalamojo <kojo22444488888888@gmail.com>
2026-08-08 04:43:16 -0400

    Direct return in async

--- src/utils/db_tables.py
+++ src/utils/db_tables.py
@@ -1,9 +1,12 @@
+from aws_lambda_powertools import Logger
 import lancedb
 from lancedb import AsyncTable
 from lancedb.index import BTree
 from lancedb.pydantic import LanceModel, Vector
 from config.constants import COVER_TABLE_NAME, TOWER_DIM, CLIP_DIM
 from typing import Optional
+
+logger = Logger()
 
 
 class Cover(LanceModel):
@@ -27,4 +30,6 @@ async def get_cover_table(db_uri: str) -> AsyncTable:
     if not id_stats:
         await cover_table.create_index("cover_id", config=BTree(), name="cover_id_idx")
 
+
+    logger.info("Cover table loaded")
     return cover_table
--- src/utils/models.py
+++ src/utils/models.py
@@ -1,12 +1,18 @@
 import asyncio
+from aws_lambda_powertools import Logger
 from onnxruntime import InferenceSession, SessionOptions
 from transformers import CLIPImageProcessorPil
+
+logger = Logger()
 
 
 async def get_model(
         model_path: str, session_options: SessionOptions, providers: list[str]
 ) -> InferenceSession:
-    return await asyncio.to_thread(get_model_sync, model_path, session_options, providers)
+    #return await asyncio.to_thread(get_model_sync, model_path, session_options, providers)
+    model = get_model_sync(model_path, session_options, providers)
+    logger.info("Model loaded. Just checking if async worked")
+    return model
 
 def get_model_sync(
         model_path: str, session_options: SessionOptions, providers: list[str]
@@ -18,7 +24,10 @@ def get_model_sync(
     )
 
 async def get_processor(processor_dir: str) -> CLIPImageProcessorPil:
-    return await asyncio.to_thread(get_processor_sync, processor_dir)
+    #return await asyncio.to_thread(get_processor_sync, processor_dir)
+    processor = get_processor_sync(processor_dir)
+    logger.info("Processor loaded. Just checking if async worked")
+    return processor
 
 def get_processor_sync(processor_dir: str) -> CLIPImageProcessorPil:
     return CLIPImageProcessorPil.from_pretrained(processor_dir)
